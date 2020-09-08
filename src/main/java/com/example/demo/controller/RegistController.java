2785147cce9a7356286702dc922b79d2669d293d
chenzhiqiang <15978760537@163.com>
2020-09-08 17:55:52 +0800

    1.实现用户登录，注册 2.需要进一步完善，然后添加readme

---
+++ src/main/java/com/example/demo/controller/RegistController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author   czq
+ * @Date 2020-09-08 15:38:13    
+ */
+@Controller
+@RequestMapping(value="/czq/regist")
+public class RegistController {
+
+	@RequestMapping(value="",method = RequestMethod.POST)
+	public ModelAndView indexData(ModelAndView modelAndView,@RequestParam("name") String name,@RequestParam("password") String password) {
+		System.out.println("校验通过： name:"+name+" 密码 ："+password);
+		modelAndView.addObject("czq", "加油");
+		modelAndView.setViewName("login");
+        return modelAndView;
+	}
+	
+	@RequestMapping(value="",method = RequestMethod.GET)
+	public ModelAndView indexData(ModelAndView modelAndView) {
+		System.out.println("跳转页面成功");
+		modelAndView.setViewName("regist");
+        return modelAndView;
+	}
+}
