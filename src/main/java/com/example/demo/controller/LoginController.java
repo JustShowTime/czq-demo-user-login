fe20719849a4346054c7296a2ee42b6c61c8aaa5
chenzhiqiang <15978760537@163.com>
2020-09-09 11:59:15 +0800

    1.优化代码，实现注册成功跳转登录，登陆成功跳转主页面

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,24 +1,25 @@
 package com.example.demo.controller;
 
-import org.junit.runners.Parameterized.Parameter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+
 /**
  * @author   czq
  * @Date 2020-09-08 15:38:13    
  */
 @Controller
-@RequestMapping(value="/czq/login")
+@RequestMapping(value="/login")
 public class LoginController {
 
+	
 	@RequestMapping(value="",method = RequestMethod.POST)
 	public ModelAndView indexData(ModelAndView modelAndView,@RequestParam("name") String name,@RequestParam("password") String password) {
-		System.out.println("校验通过： name:"+name+" 密码 ："+password);
-		modelAndView.addObject("czq", "加油");
+		System.out.println("登录通过： name:"+name+" 密码 ："+password);
+		modelAndView.addObject("czq", "czq,加油");
 		modelAndView.setViewName("index");
         return modelAndView;
 	}
--- src/main/java/com/example/demo/controller/RegistController.java
+++ src/main/java/com/example/demo/controller/RegistController.java
@@ -1,26 +1,41 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.service.UserService;
+import com.example.demo.vo.req.UserCreateReqVo;
 
 /**
  * @author   czq
  * @Date 2020-09-08 15:38:13    
  */
 @Controller
-@RequestMapping(value="/czq/regist")
+@RequestMapping(value="/regist")
 public class RegistController {
 
+	@Autowired
+	public UserService userService;
+	
 	@RequestMapping(value="",method = RequestMethod.POST)
 	public ModelAndView indexData(ModelAndView modelAndView,@RequestParam("name") String name,@RequestParam("password") String password) {
 		System.out.println("校验通过： name:"+name+" 密码 ："+password);
-		modelAndView.addObject("czq", "加油");
+		UserCreateReqVo userCreateReqVo=new UserCreateReqVo();
+		userCreateReqVo.setName(name);
+		userCreateReqVo.setPassword(password);
+		userService.createUser(userCreateReqVo);
+		modelAndView.addObject("name", userCreateReqVo.getName());
+		modelAndView.addObject("password", userCreateReqVo.getPassword());
 		modelAndView.setViewName("login");
         return modelAndView;
 	}
+	
+
 	
 	@RequestMapping(value="",method = RequestMethod.GET)
 	public ModelAndView indexData(ModelAndView modelAndView) {
