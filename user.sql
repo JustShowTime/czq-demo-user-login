edb6925627b027a205d13328c3580ef9708e049c
chenzhiqiang <15978760537@163.com>
2020-09-10 17:43:30 +0800

    1.更新.sql文件

--- user.sql
+++ user.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50731
 File Encoding         : 65001
 
-Date: 2020-09-08 17:56:41
+Date: 2020-09-10 17:42:39
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,10 +23,15 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '0',
+  `remark` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=128 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('35', 'test1', 'qwe123');
+INSERT INTO `user` VALUES ('35', '陈志强', 'qwe123', '0', '0');
+INSERT INTO `user` VALUES ('113', 'admin', 'qwe123', '0', '1');
+INSERT INTO `user` VALUES ('126', '路飞', 'qwe123', '0', '1');
+INSERT INTO `user` VALUES ('127', '索隆', 'qwe123', '0', '0');
